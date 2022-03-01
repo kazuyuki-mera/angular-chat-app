adf63ab21f6ee2822756e0c3e128da8e9fd47116
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-03-01 22:08:09 +0900

    タイムラインを表示する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '武豊', message: 'お疲れ様です！' },
+  { name: '武豊', message: 'この間の件ですが、どうなりましたか？' },
+  { name: '福永祐一', message: 'お疲れ様です！' },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chat-app';
+  comments = COMMENTS;
 }
