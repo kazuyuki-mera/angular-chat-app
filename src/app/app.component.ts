44ccd6341e35b7c15cd9a6a09520af71ec0d57f3
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-03-01 22:31:10 +0900

    クラスでデータを作成する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
 import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, '武豊');
+const ANOTHER_USER: User = new User(2, '福永祐一');
 
 const COMMENTS: Comment[] = [
-  { name: '武豊', message: 'お疲れ様です！' },
-  { name: '武豊', message: 'この間の件ですが、どうなりましたか？' },
-  { name: '福永祐一', message: 'お疲れ様です！' },
+  new Comment(ANOTHER_USER, 'お疲れ様です！'),
+  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
+  new Comment(CURRENT_USER, 'お疲れ様です！'),
+  new Comment(CURRENT_USER, 'クライアントからOKが出ました!'),
 ];
 
 @Component({
@@ -15,4 +20,5 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
   comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
